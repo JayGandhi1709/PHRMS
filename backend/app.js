aebd11d54cd5a4a246b54971ef83abc892ee8cf6
Jay Gandhi <72974975+JayGandhi1709@users.noreply.github.com>
2024-03-03 21:31:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://phrms.vercel.app",
     methods: ["GET", "POST", "DELETE", "PATCH","PUT"],
     credentials: true,
   })
