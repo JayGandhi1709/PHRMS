7fd011ff2240f70d2bbcc389dbf2be342731cadc
Jay Gandhi <72974975+JayGandhi1709@users.noreply.github.com>
2024-07-07 13:32:30 +0000

    commit

--- backend/middlewares/patientAuthMiddleware.js
+++ backend/middlewares/patientAuthMiddleware.js
@@ -4,41 +4,41 @@ const Patient = require("../models/patient");
 // app.use(cookieParser());
 
 const requirePatientAuth = async (req, res, next) => {
-  console.log("Patient Auth MiddleWare : ",req.cookies.jwtoken);
+  console.log("Patient Auth MiddleWare : ", req.cookies.jwtoken);
   try {
-  if (req.cookies.jwtoken) {
-    const token = req.cookies.jwtoken;
-    if (token) {
-      jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
-        if (err) {
-          let AuthError = { error: "Patient is not authenticated!" };
-          res.status(401).send({ AuthError });
-        } else {
-          const patient = await Patient.findById(decodedToken.id);
-          // console.log(patient);
-          if (patient) {
-            req.patient = patient;
-            next();
-          } else {
-            console.log(err);
+    if (req.cookies.jwtoken) {
+      const token = req.cookies.jwtoken;
+      if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
+          if (err) {
             let AuthError = { error: "Patient is not authenticated!" };
             res.status(401).send({ AuthError });
+          } else {
+            const patient = await Patient.findById(decodedToken.id);
+            // console.log(patient);
+            if (patient) {
+              req.patient = patient;
+              next();
+            } else {
+              console.log(err);
+              let AuthError = { error: "Patient is not authenticated!" };
+              res.status(401).send({ AuthError });
+            }
           }
-        }
-      });
+        });
+      } else {
+        let AuthError = { error: "Patient is not authenticated!" };
+        // console.log(AuthError);
+        res.status(401).send({ AuthError });
+      }
     } else {
       let AuthError = { error: "Patient is not authenticated!" };
       // console.log(AuthError);
       res.status(401).send({ AuthError });
     }
-  } else {
-    let AuthError = { error: "Patient is not authenticated!" };
-    // console.log(AuthError);
-    res.status(401).send({ AuthError });
+  } catch (error) {
+    console.log("error : ",error);
   }
-} catch (error) {
-    console.log(error);
-}
 };
 
 module.exports = { requirePatientAuth };
